d7f44add105a8f5a127c276e7e1e36393a4a5f77
google <google@gmail.com>
2016-11-08 19:03:59 +0800

    Add path: /webhook

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.get('/', function (req, res) {
 	res.send('hello world')
 })
 
+app.get('/webhook', function (req, res) {
+	res.send('webhook')
+})
+
 console.log(process.argv[2]);
 app.listen(process.argv[2])
