b2bb5cfdb60676659acba7c4682ade24a77baad9
google <google@gmail.com>
2016-11-08 19:26:06 +0800

    Add webhook method

--- app.js
+++ app.js
@@ -19,10 +19,50 @@ app.post('/profile', upload.array(), function (req, res, next) {
 	res.json(req.body);
 });
 
-app.get('/webhook', function (req, res) {
-	console.log(req.body);
-	res.json(req.body);
-})
+var CHANNEL_ID = 1487386848;
+var CHANNEL_SERECT = '0f75c2079ca3c06e972976d870718f2d';
+var MID = '';
+function sendTextMessage(sender, text) {
+	const data = {
+		to: [sender],
+		toChannel: 1383378250,
+		eventType: '138311608800106203',
+		content: {
+			contentType: 1,
+			toType: 1,
+			text: text
+		}
+	};
+
+	console.log('send: ', data);
+
+	request({
+		url: LINE_API,
+		headers: {
+			'Content-Type': 'application/json; charset=UTF-8',
+			'X-Line-ChannelID': CHANNEL_ID,
+			'X-Line-ChannelSecret': CHANNEL_SERECT//, 'X-Line-Trusted-User-With-ACL': MID
+		},
+		method: 'POST',
+		body: JSON.stringify(data)
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+		console.log('send response: ', body);
+	});
+}
+
+app.post('/webhook', (req, res) => {
+	const result = req.body.result;
+	for(let i=0; i<result.length; i++){
+		const data = result[i]['content'];
+		console.log('receive: ', data);
+		sendTextMessage(data.from, data.text);
+	}
+});
 
 console.log(process.argv[2]);
 app.listen(process.argv[2])
